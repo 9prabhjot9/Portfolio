a1a9580dd7cf5b8dac4bcbc839a5284666fd67e3
9prabhjot9 <ps459811@gmail.com>
2025-02-24 22:26:17 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,13 +14,13 @@ export function Navbar() {
       className="fixed w-full z-50 top-0 flex items-center justify-between px-4 py-4 md:px-8 glass-card"
     >
       <Link href="/" className="text-2xl font-bold gradient-text">
-        PS
+        
       </Link>
       <div className="flex items-center gap-6">
         {[
-          { href: "https://github.com/yourusername", icon: Github },
-          { href: "https://linkedin.com/in/yourusername", icon: Linkedin },
-          { href: "https://twitter.com/yourusername", icon: () => (
+          { href: "https://github.com/9prabhjot9", icon: Github },
+          { href: "https://www.linkedin.com/in/prabhjot-singh-0475ba278/", icon: Linkedin },
+          { href: "https://x.com/9PrabhjotSingh9", icon: () => (
               <motion.div className="text-white/80 hover:text-white transition-colors"  whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
                 <Image src="/twitters.png" alt="Twitter" width={24} height={24} className="w-6 h-6" />
               </motion.div>
