91066e9d960e6a3d03880683b233cf59d52c460f
Prabhjot Singh <ps459811@gmail.com>
2025-09-02 04:03:25 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -93,27 +93,15 @@ export default function Home() {
               <div className="space-y-4">
                 <div className="text-sm text-muted-foreground font-mono">CURRENTLY</div>
                 <div className="flex items-center gap-4">
-  <div className="text-foreground">Student</div>
-  <a
-    href="/resume.pdf"
-    target="_blank"
-    className="flex items-center gap-1 text-sm text-muted-foreground hover:text-foreground transition-colors"
-  >
-    <svg
-      className="w-5 h-8"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M12 2a2 2 0 012 2v16a2 2 0 01-2 2H6a2 2 0 01-2-2V4a2 2 0 012-2h6zm0 0l6 6m-6-6v6h6"
-      />
-    </svg>
-    Resume
-  </a>
+              <div className="text-foreground">Student</div>
+              <a
+                href="/resume.pdf"
+                target="_blank"
+                className="flex items-center gap-1 text-sm text-muted-foreground hover:text-foreground transition-colors"
+              >
+        <svg data-testid="geist-icon" height="16" strokeLinejoin="round" viewBox="0 0 16 16" width="16"><path fillRule="evenodd" clipRule="evenodd" d="M13.5 4.6736V9.09116L15 10.7065V4.44907C15 4.16343 14.8779 3.89142 14.6644 3.70166L10.7842 0.252591C10.6011 0.0898786 10.3647 0 10.1198 0H2.5H1V1.5V13.5C1 14.8807 2.11929 16 3.5 16H11.7275L11.3016 15.5414L10.3346 14.5H3.5C2.94772 14.5 2.5 14.0523 2.5 13.5V1.5H9.9297L13.5 4.6736ZM8 6C6.89543 6 6 6.89543 6 8C6 9.10457 6.89543 10 8 10C9.10457 10 10 9.10457 10 8C10 6.89543 9.10457 6 8 6ZM4.5 8C4.5 6.067 6.067 4.5 8 4.5C9.933 4.5 11.5 6.067 11.5 8C11.5 8.63488 11.331 9.23028 11.0354 9.74364L14.0496 12.9897L14.5599 13.5393L13.4607 14.5599L12.9504 14.0103L10.0223 10.857C9.4512 11.262 8.7534 11.5 8 11.5C6.067 11.5 4.5 9.933 4.5 8Z" fill="currentColor"></path></svg>
+        Resume
+      </a>
 </div>
 <div className="text-xs text-muted-foreground">2024 — 2028</div>
               </div>
@@ -135,7 +123,7 @@ export default function Home() {
           </div>
         </header>
 
-        <section id="work" ref={(el) => {sectionsRef.current[1] = el}} className="min-h-screen py-10 opacity-0">
+        <section id="work" ref={(el) => {sectionsRef.current[1] = el}} className="min-h-screen py-8 opacity-0">
           <div className="space-y-16">
             <div className="flex items-end justify-between">
               <h2 className="text-4xl font-light">Selected Project</h2>
